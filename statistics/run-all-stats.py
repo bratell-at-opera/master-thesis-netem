bffe4309e46d0092bbae6c7e537b6fcd0ed76e70
Hans-Filip Elo <hansfilipelo@gmail.com>
2017-03-31 09:35:55 +0200

    Scripts can create results table in one-line

--- statistics/run-all-stats.py
+++ statistics/run-all-stats.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 import subprocess
+import os
 
+
+this_folder = os.path.dirname(os.path.realpath(__file__))
+target_folder = this_folder + os.path.sep + "results" + os.path.sep
+
+if not os.path.exists(target_folder):
+    os.makedirs(target_folder)
 
 scen1 = [
     "--delay-dl=300",
@@ -52,45 +59,67 @@ scenarios.append(scen2)
 scenarios.append(scen3)
 scenarios.append(scen4)
 
-i = 0
+i = 1
 for scen in scenarios:
     # Open connections
     subprocess.check_call(
-        ["./generate-stats"] +
+        [this_folder + os.path.sep + "generate-stats"] +
         scen +
         ["--open-connection", "-h1",],
-        stdout=open("/tmp/scen" + str(i) + "_open_http1.json", "w+")
+        stdout=open(target_folder + "scen" + str(i) + "_open_http1.json", "w+")
         )
     subprocess.check_call(
-        ["./generate-stats"] +
+        [this_folder + os.path.sep + "generate-stats"] +
         scen +
         ["--open-connection", "-h2"],
-        stdout=open("/tmp/scen" + str(i) + "_open_http2.json", "w+")
+        stdout=open(target_folder + "scen" + str(i) + "_open_http2.json", "w+")
         )
     subprocess.check_call(
-        ["./generate-stats"] +
+        [this_folder + os.path.sep + "generate-stats"] +
         scen +
         ["--open-connection", "-q", "-pq"],
-        stdout=open("/tmp/scen" + str(i) + "_open_pq.json", "w+")
+        stdout=open(target_folder + "scen" + str(i) + "_open_pq.json", "w+")
         )
+    i += 1
 
+i = 1
 for scen in scenarios:
     # Closed connection
     subprocess.check_call(
-        ["./generate-stats"] +
+        [this_folder + os.path.sep + "generate-stats"] +
         scen +
         ["-h1",],
-        stdout=open("/tmp/scen" + str(i) + "_close_http1.json", "w+")
+        stdout=open(target_folder + "scen" + str(i) + "_close_http1.json", "w+")
         )
     subprocess.check_call(
-        ["./generate-stats"] +
+        [this_folder + os.path.sep + "generate-stats"] +
         scen +
         ["-h2"],
-        stdout=open("/tmp/scen" + str(i) + "_close_http2.json", "w+")
+        stdout=open(target_folder + "scen" + str(i) + "_close_http2.json", "w+")
         )
     subprocess.check_call(
-        ["./generate-stats"] +
+        [this_folder + os.path.sep + "generate-stats"] +
         scen +
         ["-q", "-pq"],
-        stdout=open("/tmp/scen" + str(i) + "_close_pq.json", "w+")
+        stdout=open(target_folder + "scen" + str(i) + "_close_pq.json", "w+")
+        )
+    i += 1
+
+for i in range(1, 5):
+    for test_type in ["close", "open"]:
+        subprocess.check_call(
+            [
+                this_folder + os.path.sep + "compare",
+                target_folder + "scen" + str(i) + "_" + test_type + "_http1.json",
+                target_folder + "scen" + str(i) + "_" + test_type + "_pq.json",
+            ],
+            stdout=open(target_folder + "comp_pq_http1_scen" + str(i) + "_" + test_type, "w+")
+        )
+        subprocess.check_call(
+            [
+                this_folder + os.path.sep + "compare",
+                target_folder + "scen" + str(i) + "_" + test_type + "_http2.json",
+                target_folder + "scen" + str(i) + "_" + test_type + "_pq.json",
+            ],
+            stdout=open(target_folder + "comp_pq_http1_scen" + str(i) + "_" + test_type, "w+")
         )
