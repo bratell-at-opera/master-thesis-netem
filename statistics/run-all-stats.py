ed091d7aa445cbfcbea0d1daf3a6036227d38fa4
Hans-Filip Elo <helo@opera.com>
2017-05-02 13:32:06 +0200

    WIP: Looking at why protocol differes

--- statistics/run-all-stats.py
+++ statistics/run-all-stats.py
@@ -2,7 +2,11 @@
 
 import subprocess
 import os
-from scenarios import *
+import sys
+from scenarios import scenarios, \
+    bandwidth_scenarios, \
+    latency_scenarios, \
+    loss_scenarios
 
 this_folder = os.path.dirname(os.path.realpath(__file__))
 target_folder = this_folder + os.path.sep + "results" + os.path.sep
@@ -10,67 +14,118 @@ target_folder = this_folder + os.path.sep + "results" + os.path.sep
 if not os.path.exists(target_folder):
     os.makedirs(target_folder)
 
-i = 1
-for scen in scenarios:
+scenarios_to_check = scenarios
+if len(sys.argv) > 1:
+    if sys.argv[1] == "--bandwidth":
+        scenarios_to_check = bandwidth_scenarios
+    elif sys.argv[1] == "--loss":
+        scenarios_to_check = loss_scenarios
+    elif sys.argv[1] == "--latency":
+        scenarios_to_check = latency_scenarios
+    else:
+        scenarios_to_check = scenarios
+
+
+for scen in scenarios_to_check:
     # Open connections
     subprocess.check_call(
         [this_folder + os.path.sep + "generate-stats"] +
-        scen +
-        ["--open-connection", "-h1",],
-        stdout=open(target_folder + "scen" + str(i) + "_open_http1.json", "w+")
+        scen[1] +
+        ["--open-connection", "-h1"],
+        stdout=open(
+            target_folder +
+            scen[0] +
+            "_open_http1.json", "w+")
         )
     subprocess.check_call(
         [this_folder + os.path.sep + "generate-stats"] +
-        scen +
+        scen[1] +
         ["--open-connection", "-h2"],
-        stdout=open(target_folder + "scen" + str(i) + "_open_http2.json", "w+")
+        stdout=open(
+            target_folder +
+            scen[0] +
+            "_open_http2.json", "w+")
         )
     subprocess.check_call(
         [this_folder + os.path.sep + "generate-stats"] +
-        scen +
+        scen[1] +
         ["--open-connection", "-q", "-pq"],
-        stdout=open(target_folder + "scen" + str(i) + "_open_pq.json", "w+")
+        stdout=open(
+            target_folder +
+            scen[0] +
+            "_open_pq.json", "w+")
         )
-    i += 1
 
-i = 1
-for scen in scenarios:
+for scen in scenarios_to_check:
     # Closed connection
     subprocess.check_call(
         [this_folder + os.path.sep + "generate-stats"] +
-        scen +
-        ["-h1",],
-        stdout=open(target_folder + "scen" + str(i) + "_close_http1.json", "w+")
+        scen[1] +
+        ["-h1"],
+        stdout=open(
+            target_folder +
+            scen[0] +
+            "_close_http1.json", "w+")
         )
     subprocess.check_call(
         [this_folder + os.path.sep + "generate-stats"] +
-        scen +
+        scen[1] +
         ["-h2"],
-        stdout=open(target_folder + "scen" + str(i) + "_close_http2.json", "w+")
+        stdout=open(
+            target_folder +
+            scen[0] +
+            "_close_http2.json", "w+")
         )
     subprocess.check_call(
         [this_folder + os.path.sep + "generate-stats"] +
-        scen +
+        scen[1] +
         ["-q", "-pq"],
-        stdout=open(target_folder + "scen" + str(i) + "_close_pq.json", "w+")
+        stdout=open(target_folder + scen[0] + "_close_pq.json", "w+")
         )
-    i += 1
 
-for i in range(1, len(scenarios)+1):
+for scen in scenarios_to_check:
     for test_type in ["close", "open"]:
         subprocess.check_call(
             [
                 this_folder + os.path.sep + "compare",
-                target_folder + "scen" + str(i) + "_" + test_type + "_http1.json",
-                target_folder + "scen" + str(i) + "_" + test_type + "_pq.json",
+                target_folder +
+                scen[0] +
+                "_" +
+                test_type +
+                "_http1.json",
+                target_folder +
+                scen[0] +
+                "_" +
+                test_type +
+                "_pq.json",
             ],
-            stdout=open(target_folder + "comp_pq_http1_scen" + str(i) + "_" + test_type + ".txt", "w+")
+            stdout=open(
+                target_folder +
+                "comp_pq_http1_scen" +
+                scen[0] +
+                "_" +
+                test_type +
+                ".txt", "w+")
         )
         subprocess.check_call(
             [
                 this_folder + os.path.sep + "compare",
-                target_folder + "scen" + str(i) + "_" + test_type + "_http2.json",
-                target_folder + "scen" + str(i) + "_" + test_type + "_pq.json",
+                target_folder +
+                scen[0] +
+                "_" +
+                test_type +
+                "_http2.json",
+                target_folder +
+                scen[0] +
+                "_" +
+                test_type +
+                "_pq.json",
             ],
-            stdout=open(target_folder + "comp_pq_http2_scen" + str(i) + "_" + test_type + ".txt", "w+")
+            stdout=open(
+                target_folder +
+                "comp_pq_http2_scen" +
+                scen[0] +
+                "_" +
+                test_type +
+                ".txt", "w+")
         )
--- statistics/scenarios.py
+++ statistics/scenarios.py
@@ -1,102 +1,136 @@
 #!/usr/bin/env python3
 
 # Unlimited
-scen8 = [
-]
+scen8 = (
+    "scen8",
+    [])
 
 # Bandwidth scenarios
-scen10 = [
-    "--bandwidth-dl=2",
-    "--bandwidth-ul=1"
-]
-scen11 = [
-    "--bandwidth-dl=0.5",
-    "--bandwidth-ul=0.25"
-]
-scen12 = [
-    "--trace-multiplyer-ul=1.2",
-    "--trace-multiplyer-dl=2.4",
-    "--bw-trace=udp-traces/storo_4x_700kbps_120s_tv.log"
-]
-scen13 = [
-    "--trace-multiplyer-ul=0.6",
-    "--trace-multiplyer-dl=1.2",
-    "--bw-trace=udp-traces/storo_4x_700kbps_120s_tv.log"
-]
-scen14 = [
-    "--bandwidth-dl=5",
-    "--bandwidth-ul=3"
-]
-scen15 = [
-    "--trace-multiplyer-ul=0.3",
-    "--trace-multiplyer-dl=0.6",
-    "--bw-trace=udp-traces/storo_4x_700kbps_120s_tv.log"
-]
+scen10 = (
+    "scen10",
+    [
+        "--bandwidth-dl=2",
+        "--bandwidth-ul=1"
+    ])
+
+scen11 = (
+    "scen11",
+    [
+        "--bandwidth-dl=0.5",
+        "--bandwidth-ul=0.25"
+    ])
+
+scen12 = (
+    "scen12",
+    [
+        "--trace-multiplyer-ul=1.2",
+        "--trace-multiplyer-dl=2.4",
+        "--bw-trace=udp-traces/storo_4x_700kbps_120s_tv.log"
+    ])
+scen13 = (
+    "scen13",
+    [
+        "--trace-multiplyer-ul=0.6",
+        "--trace-multiplyer-dl=1.2",
+        "--bw-trace=udp-traces/storo_4x_700kbps_120s_tv.log"
+    ])
+scen14 = (
+    "scen14",
+    [
+        "--bandwidth-dl=5",
+        "--bandwidth-ul=3"
+    ])
+scen15 = (
+    "scen15",
+    [
+        "--trace-multiplyer-ul=0.3",
+        "--trace-multiplyer-dl=0.6",
+        "--bw-trace=udp-traces/storo_4x_700kbps_120s_tv.log"
+    ])
 
 # Latency scenarios
-scen20 = [
-    "--delay-dl=300",
-    "--delay-ul=300",
-    "--bandwidth-dl=10",
-    "--bandwidth-ul=10"
-]
-scen21 = [
-    "--delay-dl=150",
-    "--delay-ul=150",
-    "--bandwidth-dl=10",
-    "--bandwidth-ul=10"
-]
-scen22 = [
-    "--delay-dl=75",
-    "--delay-ul=75",
-    "--bandwidth-dl=10",
-    "--bandwidth-ul=10"
-]
-scen22 = [
-    "--delay-dl=50",
-    "--delay-ul=50",
-    "--bandwidth-dl=10",
-    "--bandwidth-ul=10"
-]
+scen20 = (
+    "scen20",
+    [
+        "--delay-dl=300",
+        "--delay-ul=300",
+        "--bandwidth-dl=10",
+        "--bandwidth-ul=10"
+    ])
+scen21 = (
+    "scen21",
+    [
+        "--delay-dl=150",
+        "--delay-ul=150",
+        "--bandwidth-dl=10",
+        "--bandwidth-ul=10"
+    ])
+scen22 = (
+    "scen22",
+    [
+        "--delay-dl=75",
+        "--delay-ul=75",
+        "--bandwidth-dl=10",
+        "--bandwidth-ul=10"
+    ])
+scen23 = (
+    "scen23",
+    [
+        "--delay-dl=50",
+        "--delay-ul=50",
+        "--bandwidth-dl=10",
+        "--bandwidth-ul=10"
+    ])
 
 # Latency variation scenario
-scen30 = [
-    "--delay-dl=50",
-    "--delay-ul=50",
-    "--delay-deviation-ul=0.1",
-    "--delay-deviation-dl=2.5",
-    "--bandwidth-dl=10",
-    "--bandwidth-ul=10"
-]
+scen30 = (
+    "scen30",
+    [
+        "--delay-dl=50",
+        "--delay-ul=50",
+        "--delay-deviation-ul=0.1",
+        "--delay-deviation-dl=2.5",
+        "--bandwidth-dl=10",
+        "--bandwidth-ul=10"
+    ])
 
 # Loss scenarios
-scen40 = [
-    "--bandwidth-dl=10",
-    "--bandwidth-ul=10",
-    "--loss-prob-move-to-burst-dl=1",
-    "--loss-prob-move-to-gap-dl=3",
-    "--loss-rate-burst-dl=70",
-]
-scen41 = [
-    "--bandwidth-dl=10",
-    "--bandwidth-ul=10",
-    "--loss-prob-move-to-burst-dl=1",
-    "--loss-prob-move-to-gap-dl=3",
-    "--loss-rate-burst-dl=35",
-]
-scen42 = [
-    "--bandwidth-dl=10",
-    "--bandwidth-ul=10",
-    "--loss-prob-move-to-burst-dl=1",
-    "--loss-prob-move-to-gap-dl=3",
-    "--loss-rate-burst-dl=10",
-]
+scen40 = (
+    "scen40",
+    [
+        "--bandwidth-dl=10",
+        "--bandwidth-ul=10",
+        "--loss-prob-move-to-burst-dl=1",
+        "--loss-prob-move-to-gap-dl=3",
+        "--loss-rate-burst-dl=70",
+    ])
+scen41 = (
+    "scen41",
+    [
+        "--bandwidth-dl=10",
+        "--bandwidth-ul=10",
+        "--loss-prob-move-to-burst-dl=1",
+        "--loss-prob-move-to-gap-dl=3",
+        "--loss-rate-burst-dl=35",
+    ])
+scen42 = (
+    "scen42",
+    [
+        "--bandwidth-dl=10",
+        "--bandwidth-ul=10",
+        "--loss-prob-move-to-burst-dl=1",
+        "--loss-prob-move-to-gap-dl=3",
+        "--loss-rate-burst-dl=10",
+    ])
 
 scenarios_defined = globals()
 scenarios = []
 bandwidth_scenarios = []
+bandwidth_scenarios.append(scen8)
 latency_scenarios = []
+latency_scenarios.append(scen8)
 loss_scenarios = []
+loss_scenarios.append(scen8)
 
 for i in range(1, 50):
     var_name = "scen" + str(i)
@@ -117,4 +151,3 @@ for i in range(40, 50):
     var_name = "scen" + str(i)
     if var_name in scenarios_defined:
         loss_scenarios.append(scenarios_defined[var_name])
-
