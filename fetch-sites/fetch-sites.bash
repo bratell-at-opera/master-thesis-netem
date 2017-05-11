3f2a9c39f10bfe36a0560625d1b14129af2858d5
hansfilipelo <hansfilipelo@gmail.com>
2017-05-11 13:10:26 +0200

    Fixed graph scaling

--- fetch-sites/fetch-sites.bash
+++ fetch-sites/fetch-sites.bash
@@ -8,6 +8,6 @@ urlFile=$netemFolder/config/urls.txt
 # Save all sites
 echo "Starting!"
 echo "----------------"
-wget --page-requisites --html-extension --convert-links --span-hosts --no-clobber --tries=2 --timeout=10 --directory-prefix=$webRoot --random-wait --wait 1 --input-file=$urlFile
+wget --no-check-certificate --page-requisites --html-extension --convert-links --span-hosts --no-clobber --tries=2 --timeout=10 --directory-prefix=$webRoot --random-wait --wait 1 --input-file=$urlFile
 echo "----------------"
 echo ""
