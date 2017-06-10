0a96e84ab4ed975a25d31466228444901b0215f0
Hans-Filip Elo <hansfilipelo@gmail.com>
2017-06-10 09:03:05 +0200

    Temporary change in order to measure the data I've fetched.

--- fetch-sites/fetch-sites.bash
+++ fetch-sites/fetch-sites.bash
@@ -2,12 +2,17 @@
 
 netemFolder=$(realpath $(dirname $0)/..)
 thisFolder=$netemFolder/fetch-sites
-webRoot=$netemFolder/webroot
+#webRoot=$netemFolder/webroot
+webRoot=~/Downloads/measureData
 urlFile=$netemFolder/config/urls.txt
 
+mkdir -p $webRoot
 # Save all sites
 echo "Starting!"
 echo "----------------"
-wget --no-check-certificate --page-requisites --html-extension --convert-links --span-hosts --no-clobber --tries=2 --timeout=10 --directory-prefix=$webRoot --random-wait --wait 1 --input-file=$urlFile
+for url in $(cat $urlFile)
+do
+    wget --no-check-certificate --page-requisites --html-extension --convert-links --span-hosts --no-clobber --tries=2 --timeout=10 --directory-prefix=$webRoot/$url --random-wait --wait 1 http://localhost/$url
+done
 echo "----------------"
 echo ""
